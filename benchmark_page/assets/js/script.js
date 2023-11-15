8525f25c114af24685758dc4dd4ba3b3d9686913
Milena Livian <milena.livian@gmail.com>
2023-11-15 11:17:09 +0100

    commit

---
+++ benchmark_page/assets/js/script.js
@@ -0,0 +1,10 @@
+let timeLeft = 10; 
+let timerId = setInterval(countdown, 1000);
+function countdown() {
+    if (timeLeft == -1) {
+        clearTimeout(timerId);
+    } else {
+        console.log(timeLeft + ' seconds');
+        timeLeft--;
+    }
+}
