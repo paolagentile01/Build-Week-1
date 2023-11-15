5a3abbfc93e9320c9ad6d23425dfee34874a148f
paolagentile01 <paolagentile01@libero.it>
2023-11-15 21:10:50 +0100

    commit

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -10,3 +10,49 @@ function countdown() {
 }
 
 
+(function () {
+    var canvas  = document.getElementById('stopwatch'),
+        ctx     = canvas.getContext('2d'),
+        digital = document.getElementById('time'),
+        reset   = document.getElementById('reset'), 
+        timerOn = false,
+        second  = 2 * Math.PI / 40,
+        start   = 1.5*Math.PI,
+        t = 0,
+        animation;
+  
+    var draw = function () {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.lineWidth = 30;
+      ctx.strokeStyle = '#3399cc';
+      ctx.beginPath();
+      ctx.arc(100, 100, 80,start, start + second * t);
+      ctx.stroke();
+      t = t >= 40 ? 0: t + 0.05; 
+    }
+    
+    canvas.addEventListener('click', function () {
+      if (!timerOn) {
+        timerOn = true;
+        animation = setInterval(function () {
+          draw();
+          digital.innerHTML = Math.floor(t);
+        }, 30);
+      }
+      else {
+        timerOn = false;
+        clearInterval(animation);
+      }
+  
+    });
+    reset.addEventListener('click', function () {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      digital.innerHTML = 0;
+      timerOn = false;
+      clearInterval(animation);
+      t = 0;
+    });
+  })();
+
+
+
