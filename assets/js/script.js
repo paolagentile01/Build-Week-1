6d12031757ace4842c8f9ddec940ef799b7f281e
paolagentile01 <paolagentile01@libero.it>
2023-11-16 14:08:21 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,11 @@ box.addEventListener('change',function(){
 */
 /*ATTENZIONE:occorre cambiare percorso allo script di html di pag 1 per agganciarlo a questo js*/
 /*1)ho selezionato gli elements check-box e a dalla main page*/
+const bottoneStart = document.getElementById("fakeButton1");
 const box=document.getElementById("checkboxP1");
 const link=document.getElementById("linkP1");
+
+bottoneStart.classList.add("bottone-disabled");
 link.classList.add('disabled');
 link.removeAttribute("href",'pagina2.html');
 /*ho aggiunto un  evento in ascolto al box che fa cambiare stato al link nel button.
@@ -22,8 +25,15 @@ al link e relativo link (fittizio) e viene rimossa la classe disabilita.
 se il box non viene selezionato accade l'opposto*/ 
 box.addEventListener('change',function(){
     if(this.checked){
-    link.setAttribute('href','pagina2.html');
-    link.classList.remove('disabled')
-    }else{ link.removeAttribute("href",'pagina2.html');
-    link.classList.add('disabled')}
+        link.setAttribute('href','pagina2.html');
+        link.classList.remove('disabled')
+        bottoneStart.classList.remove("bottone-disabled");
+        bottoneStart.classList.add("bottone-active");
+    }else{ 
+        link.removeAttribute("href",'pagina2.html');
+        link.classList.add('disabled')
+        bottoneStart.classList.remove("bottone-active");
+        bottoneStart.classList.add("bottone-disabled");}
 });
+
+
