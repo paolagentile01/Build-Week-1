4d58772d290fc89400914b098926e3a79475867d
Esperanza <esperanza.llontop27@gmail.com>
2023-11-16 10:31:17 +0100

    add function the message js

--- assets/js/scriptPageResult.js
+++ assets/js/scriptPageResult.js
@@ -1,11 +1,19 @@
-let resultTest = 66.7;
+let resultTest = 40;
+let failed = "hai fallito";
 let congratulazioni = "Congratulation !";
 
-function progressBar(resultVal, totalPercentageVal ) {
+function progressBar(resultVal, totalPercentageVal) {
   let strokeVal = (5.7 * 200) / totalPercentageVal;
   let x = document.querySelector(".outer");
   x.style.strokeDasharray = resultVal * strokeVal + " 999";
-  let el = document.querySelector(".text");
- el.innerHTML = `${congratulazioni}`;
 }
 progressBar(resultTest, 100);
+let el = document.querySelector(".text");
+function message(result) {
+  if (result > 60) {
+    return (el.innerHTML = `${congratulazioni}`);
+  } else {
+    return (el.innerHTML = `${failed}`);
+  }
+}
+ message(resultTest)
