1079ac944db988d692f8e1bac167aa1bf156a5a1
paolagentile01 <paolagentile01@libero.it>
2023-11-17 16:42:00 +0100

    commit

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,19 +1,13 @@
-/*per ogni volta che passiamo il mouse sopra le stelline inerenti la recensione
-cambierà il colore*/
-function cambioColore() {
-    let stelline = document.querySelectorAll(".stellineFeedback");
-    stelline.forEach(function(stella) {
-        stella.addEventListener("mouseover", function(e) {
-            e.target.style.color = "aqua";
-        });
-        stella.addEventListener("mouseout", function(e) {
-            e.target.style.border = "none";
-        });
-        stella.addEventListener("onclick",function(e){
-            e.target.style.border ="2px solid aqua";
-        });
-    });
-}
 
-cambioColore();
+    function coloraStelle(numeroStella) {
+    var stelle = document.querySelectorAll('.star1');
+
+    for (var i = 0; i < stelle.length; i++) {
+        if (i < numeroStella) {
+            stelle[i].style.color = 'gold';
+        } else {
+            stelle[i].style.color = 'black';
+        }
+    }
+};
 
