5249c1e0da1a4fa18aa39f1d63a97e675d291c2a
lorenzovitale03 <lorenzo.vitale2003@outlook.it>
2023-11-20 16:39:19 +0100

    commit per prendere l'ultimo  main

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,15 +1,5 @@
 /*per ogni volta che passiamo il mouse sopra le stelline inerenti la recensione
 cambierà il colore*/
-const stars=document.querySelectorAll(".stellineFeedback");
-function cambiaColore(array,numero){
-    console.log(stars);
-    for(let i = 0; array.length-numero>i; i++){
-        array[i].classList.add("stellineFeedback1");
-    }
-}
-
-cambiaColore(stars,4);
-
     function coloraStelle(numeroStella) {
     var stelle = document.querySelectorAll('.star1');
 
